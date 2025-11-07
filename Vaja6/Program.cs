09bbb326518fa82a22a91811bae5be252a6b959b
ozbejzurman10 <ozbej.zurman10@gmail.com>
2025-11-07 09:28:10 +0100

    Spremenil pod v PRIVATE

--- Vaja6/Program.cs
+++ Vaja6/Program.cs
@@ -9,10 +9,11 @@ namespace vaja6 {
 
 	// Se en komentar
     class Igralec {
-        public int id;
-        public string u_ime;
-        public string u_geslo;
-        public static int stIg = 1;
+
+        private int id;
+        private string u_ime;
+        private string u_geslo;
+        private static int stIg = 1;
 
 		// Provzeti konstruktor
         public Igralec() {
